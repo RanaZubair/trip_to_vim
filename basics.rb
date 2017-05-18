bff6d3195b3d2a14862f76d8b694ceed564870dd
RanaZubair <muhammadzubairsabir@gmail.com>
2017-05-18 23:56:48 +0500

    vim basics has been added

---
+++ basics.rb
@@ -0,0 +1,42 @@
+#These are basics of vim
+
+=begin
+
+..... General information about vim.....
+
+vi is created by bram Moolenaar
+gvim stands for graphical vim used mostly in windows os environment
+
+Here i will discuss how to use vim in linux environment
+
+1. Type vim in terminal it will prompt vim
+2. There are two modes in vim insert mode and command mode.
+3. By default vim have command mode 
+4. Type i to enter into the insert mode.
+5. Buffer in vim is a place in memory which holds the information
+   that we are typing.
+6. You can later save this to a file and a buffer can hold the 
+   content of that file.
+7. Type ESC key to goto the command mode
+ 
+
+..... Write commands .....
+
+1. :w filename+ext or :write filename+ext
+   1L , 40C (L for line and C for column)
+
+..... Quit vim .....
+
+1. :q or :quit to quit from vim :wq is used for write and quit.
+2. :q! is used to quit without warning :wq! is used for write 
+   quit without warning
+
+
+..... Moving VIM .....
+
+1. J key moves one line down.
+2. K key moves one line up.
+3. H key moves to left one alphabet.
+4. L key moves to right one alphabet .
+
+=end
