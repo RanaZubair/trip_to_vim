e7f58e1f5ece060d4bfe90f23da2cec5225a28ab
RanaZubair <muhammadzubairsabir@gmail.com>
2017-05-24 00:54:10 +0500

    movement file is initialized

---
+++ movement.rb
@@ -0,0 +1,40 @@
+#This is movement file
+
+=begin
+
+1. ctrl+d (moves down half of the screen)
+   ctrl+u is used to move up.
+
+2. ctrl+d moves one entire screen down
+   ctrl+b b for backward moves entire screen backward.
+
+3. Big M moves to the middle of the screen from the bottom.
+
+4. Big H will goto the top of the window.
+
+5. Big L or Shift+l moves to the bottom of the window.
+
+   Add repeators to L command 3L moves 3 lines above the bottom.
+
+   3H moves 3 line from the top of the window.
+
+7. 6M will not work.
+
+8. Press zt to move a line of the method at the top of screen.
+
+9. Press zb used for the bottom.
+
+10.Press zz to move line in the middle of the window.
+
+11.We can quickly use markers to navigate from one line to another.
+
+12.Press m and name of the register marker of line will be needed.
+  
+   ma ,  'a you will goto line.
+
+   " single quote to goto the last position.
+
+
+
+
+=end
